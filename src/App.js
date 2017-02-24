1a3d7a107ef1617260d120032ed0be48e373c3a5
Vidsneezes <moraleszez@gmail.com>
2017-02-24 16:23:02 -0600

    added searchbar component in app js

--- src/App.js
+++ src/App.js
@@ -42,10 +42,26 @@ class ProductTable extends Component{
   }
 }
 
+class SearchBar extends Component{
+  render(){
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
 class FilterableProductTable extends Component {
   render() {
     return (
       <div>
+        <SearchBar />
         <ProductTable products={this.props.products}/>
       </div>
     );
