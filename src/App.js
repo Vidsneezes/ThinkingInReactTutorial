35f89e209550b9c0be1102817c7a30b94dc45feb
Vidsneezes <moraleszez@gmail.com>
2017-02-24 16:09:20 -0600

    added static components productrow productcategoryrow to app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+class ProductCategoryRow extends Component{
+  render(){
+    return <tr><th colSpan="2">{this.props.category}</th></tr>;
+  }
+}
+
+class ProductRow extends Component{
+  render(){
+    var name = this.props.product.stocked ? this.props.product.name : <span style={{color:'red'}}>{this.props.product.name}</span>;
+    return (
+      <tr>
+       <td>{name}</td>
+       <td>{this.props.product.price}</td>
+      </tr>
+    );
+  }
+}
+
+class FilterableProductTable extends Component {
   render() {
     return (
       <div>
@@ -9,4 +27,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default FilterableProductTable;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import FilterableProductTable from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <FilterableProductTable />,
   document.getElementById('root')
 );
