4146f35c8028dab630f85dcd8563c99835713ee5
Vidsneezes <moraleszez@gmail.com>
2017-02-24 16:20:03 -0600

    added product table to app js and product array to index js

--- src/App.js
+++ src/App.js
@@ -18,10 +18,35 @@ class ProductRow extends Component{
   }
 }
 
+class ProductTable extends Component{
+  render(){
+    var rows = [];
+    var lastCategory = null;
+    this.props.products.forEach(function(product){
+      if(product.category !== lastCategory){
+        rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
+    
+      }
+      rows.push(<ProductRow product={product} key={product.name}/>);
+      lastCategory = product.category;
+    });
+    return (
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Price</th>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
 class FilterableProductTable extends Component {
   render() {
     return (
       <div>
+        <ProductTable products={this.props.products}/>
       </div>
     );
   }
