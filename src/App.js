a0e3a9557a25b80cd68ac7207cb3e7a16b721c6a
Vidsneezes <moraleszez@gmail.com>
2017-02-24 16:33:01 -0600

    added body padding to index css, added constructor infilterableproducttable component in app js

--- src/App.js
+++ src/App.js
@@ -58,11 +58,26 @@ class SearchBar extends Component{
 }
 
 class FilterableProductTable extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+  }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <ProductTable products={this.props.products}/>
+        <SearchBar 
+          filterText = {this.state.filterText}
+          inStockOnly = {this.state.inStockOnly}
+        />
+        <ProductTable 
+          products={this.props.products}
+          filterText = {this.state.filterText}
+          inStockOnly = {this.state.inStockOnly}
+        />
       </div>
     );
   }
